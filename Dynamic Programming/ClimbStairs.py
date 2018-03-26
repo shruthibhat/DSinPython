5b837c0d67fc0000ad2044469f9fa0cf76e11502
Shruthi <shruthi.mlr@gmail.com>
2018-03-25 17:02:28 -0700

    Adding a folder

---
+++ Dynamic Programming/ClimbStairs.py
@@ -0,0 +1,26 @@
+"""
+You are climbing a staircase. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to
+the top?
+
+Solution : o(n) runtime ,O(1) space - Dynamic Programming
+
+"""
+
+
+def climb_stairs(n):
+    if n == 1:
+        return 1
+    a = 1
+    b = 2
+    for i in range(3, n+1):
+        temp = b
+        b = b + a
+        a = temp
+    return b
+
+
+print("The distinct ways 1 stairs can be climbed are:", climb_stairs(1))
+print("The distinct ways 2 stairs can be climbed are:", climb_stairs(2))
+print("The distinct ways 6 stairs can be climbed are:", climb_stairs(6))
+
